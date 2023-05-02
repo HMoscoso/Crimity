1d9faf196d2e74f6f3a53a4b8d3808ee757b7ce6
Andrew <andrew_programador@hotmail.com>
2023-05-02 09:32:06 -0500

    commit api login

--- lib/login/Login_start.dart
+++ lib/login/Login_start.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:http/http.dart' as http;
 
 import '../home/home_ciudadano.dart';
 
